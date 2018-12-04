959a816335e0eb7bdaf7e4627b4e63d3ebbb37b6
Simon <simonren@jidouauto.com>
2018-12-04 16:17:10 +0800

    add check

--- KotlinBase/src/main/java/com/simon/java/utils/StringTest.java
+++ KotlinBase/src/main/java/com/simon/java/utils/StringTest.java
@@ -1,17 +1,39 @@
 package com.simon.java.utils;
 
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
-import io.reactivex.Observable;
+import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
+import com.sun.org.apache.xml.internal.security.utils.Base64;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringTest {
     public static void main(String[] args) {
 
-        String returnValue = new String(Base64.decode("eyJkb29ydW5sb2NrIjoiZmFpbGVkIn0="));
-        System.out.println(returnValue);
+        String speChat = "[`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+        Pattern pattern = Pattern.compile(speChat);
+        Matcher matcher = pattern.matcher("hhjk   .");
+        System.out.println(matcher.find());
 
-        String mac = "F3:88:A9:7A:BE:61";
-        String str = mac.replace(":", "");
-        System.out.println(mac + "========" + str);
-        Observable<String> helloWorld = Observable.just("hello world");
+        String returnValue = null;
+        try {
+            returnValue = new String(Base64.decode("JA1BtfFuS8eLz2/wQy/8wudOCNGwLpjmvCOXreOlV4/gWLslTUcGPVbn8px4gCtffK2FSQvqHqIrPFaixpgka2+Hb8DrA93A7DlAUaEkx2oOA5AgNTdFKZRqM6ocQSnMjYMbY0mJDH0Mm/1jU5T55uTHDvwmRQ12jQLodwIQcao="));
+        } catch (Base64DecodingException e) {
+            e.printStackTrace();
+        }
+        System.out.println(returnValue);
+    }
+
+    public static void checkMac() {
+        String mac = "D3:41:89:A7:05:C8";
+        String[] split = mac.split(":");
+        String majorHEX = "41" + split[3];
+        String minorHEX = split[4] + split[5];
+        System.out.println(mac + "====majorHEX====" + majorHEX);
+        System.out.println(mac + "====minorHEX====" + minorHEX);
+        Integer major = Integer.parseInt(majorHEX, 16);
+        Integer minor = Integer.parseInt(minorHEX, 16);
+        System.out.println(mac + "====major====" + major);
+        System.out.println(mac + "====minor====" + minor);
+
     }
 }
