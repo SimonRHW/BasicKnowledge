5b115a91741407dae259fc639f4766f8879ee758
Simon <simonren@jidouauto.com>
2019-09-10 19:14:25 +0800

    add functional

---
+++ src/main/java/com/simon/java/functional/Either.java
@@ -0,0 +1,30 @@
+package com.simon.java.functional;
+
+public class Either<L,R> {
+
+    private L left = null;
+    private R right = null;
+    private Either(L left,R right){
+
+    }
+
+    public L getLeft(){
+        return  left;
+    }
+
+    public R getRight(){
+        return right;
+    }
+
+    public boolean isLeft(){
+        return  left != null;
+    }
+
+    public boolean isRight(){
+        return right != null;
+    }
+
+    public static <L,R> Either<L,R> right(R right){
+        return new Either<L,R>(null,right);
+    }
+}
---
+++ src/main/java/com/simon/java/functional/FunctionalTest.java
@@ -0,0 +1,26 @@
+package com.simon.java.functional;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FunctionalTest {
+
+    public static void main(String[] Args) {
+        Map<String, Object> result = divide(4, 2);
+        System.out.println((double) result.get("answer"));
+
+        Map<String, Object> result1 = divide(4, 0);
+        System.out.println((Exception)result1.get("exception"));
+
+    }
+
+    public static Map<String, Object> divide(int x, int y) {
+        Map<String, Object> result = new HashMap<>();
+        if (y == 0) {
+            result.put("exception", new Exception("div by zero"));
+        } else {
+            result.put("answer", (double) x / y);
+        }
+        return result;
+    }
+}
