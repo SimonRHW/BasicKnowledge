241694a981f0ed21259e0e0a4b5a45eee7720a60
Simon <207651411@qq.com>
2019-12-28 13:12:19 +0800

    modify gradle

---
+++ src/main/java/com/simon/java/leetcode/LeetCode1.java
@@ -0,0 +1,34 @@
+package com.simon.java.leetcode;
+
+import java.util.HashMap;
+
+public class LeetCode1 {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 5, 9, 10};
+        int[] sum = sum(nums, 10);
+        for (int item : sum) {
+            System.out.println(item);
+        }
+
+    }
+
+    public static int[] sum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        if (nums == null || nums.length <= 0)
+            return res;
+        for (int i = 0; i < nums.length - 1; i++) {
+            int num = nums[i];
+            int temp = target - num;
+            if (hashMap.containsKey(temp)) {
+                res[0] = i;
+                res[1] = hashMap.get(temp);
+            } else {
+                hashMap.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+
+}
---
+++ src/main/java/com/simon/java/leetcode/LeetCode2.java
@@ -0,0 +1,45 @@
+package com.simon.java.leetcode;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class LeetCode2 {
+
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(2);
+        ListNode listNode1 = new ListNode(5);
+        ListNode listNode2 = new ListNode(6);
+        ListNode listNode3 = new ListNode(9);
+        ListNode listNode4 = new ListNode(3);
+        ListNode listNode5 = new ListNode(8);
+
+        listNode.next = listNode1;
+        listNode1.next = listNode2;
+
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+
+    }
+
+    public static ListNode sum(ListNode listNode, ListNode otherListNode) {
+        // TODO: 2019/9/18 第一步 反转链表   第二步 比较链表长度   第三步 遍历链表（遍历链表较长的那个） 由低位同位相加 超过10 temp记录 加入下一位计算 添加到链表中 直至遍历结束  第四步 再次反转结果链表返回
+
+
+        return null;
+    }
+
+
+    //Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+}
