f514cb4724886f7c36a0952e0b254530e04f4fdd
Simon <207651411@qq.com>
2020-07-11 16:42:08 +0800

    udpate code sca

---
+++ src/main/java/com/simon/java/sourcecodeanalysis/HashMapSca.java
@@ -0,0 +1,107 @@
+package com.simon.java.sourcecodeanalysis;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * 在 JDK 1.7 中 HashMap 是以数组加链表的形式组成的，JDK 1.8 之后新增了红黑树的组成结构，当链表大于 8 并且容量大于 64 时，链表结构会转换成红黑树结构
+ * 之所以添加红黑树是因为一旦链表过长，会严重影响 HashMap 的性能，而红黑树具有快速增删改查的特点，这样就可以有效的解决链表过长时操作比较慢的问题
+ * HashMap三个比较重要的方法：查询、新增、扩容
+ */
+public class HashMapSca {
+
+    public static void main(String[] args) {
+
+        var hashMap = new HashMap<String, String>();
+
+        /*
+        HashMap 是如何导致死循环的？
+        以 JDK 1.7 为例，假设 HashMap 默认大小为 2，原本 HashMap 中有一个元素 key(5)，我们再使用两个线程：t1 添加元素 key(3)，
+        t2 添加元素 key(7)，当元素 key(3) 和 key(7) 都添加到 HashMap 中之后，
+        线程 t1 在执行到 Entry<K,V> next = e.next; 时，交出了 CPU 的使用权
+        那么此时线程 t1 中的 e 指向了 key(3)，而 next 指向了 key(7) ；
+        之后线程 t2 重新 rehash 之后链表的顺序被反转，链表的位置变成了 key(5) → key(7) → key(3)，
+        其中 “→” 用来表示下一个元素。当 t1 重新获得执行权之后，先执行 newTalbe[i] = e 把 key(3) 的 next 设置为 key(7)，
+        而下次循环时查询到 key(7) 的 next 元素为 key(3)，于是就形成了 key(3) 和 key(7) 的循环引用，因此就导致了死循环的发生
+
+        当然发生死循环的原因是 JDK 1.7 链表插入方式为首部倒序插入，这个问题在 JDK 1.8 得到了改善，变成了尾部正序插入；
+        因为 HashMap 本身就是非线程安全的，如果要在多线程下，建议使用 ConcurrentHashMap 替代
+
+        void transfer(Entry[] newTable, boolean rehash) {
+        int newCapacity = newTable.length;
+        for (Entry<K,V> e : table) {
+            while(null != e) {
+                Entry<K,V> next = e.next; // 线程一执行此处
+                if (rehash) {
+                    e.hash = null == e.key ? 0 : hash(e.key);
+                }
+                int i = indexFor(e.hash, newCapacity);
+                e.next = newTable[i];
+                newTable[i] = e;
+                e = next;
+            }
+            }
+        }
+        */
+
+
+    }
+
+
+    /**
+     * 哈希桶 每个桶包含四个字段 hash、key、value、next 表示链表的下一个节点
+     *
+     * @param <K>
+     * @param <V>
+     */
+    static class Node<K, V> implements Map.Entry<K, V> {
+
+        final int hash;
+        final K key;
+        V value;
+        HashMapSca.Node<K, V> next;
+
+        Node(int hash, K key, V value, HashMapSca.Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public final K getKey() {
+            return key;
+        }
+
+        public final V getValue() {
+            return value;
+        }
+
+        public final String toString() {
+            return key + "=" + value;
+        }
+
+        public final int hashCode() {
+            return Objects.hashCode(key) ^ Objects.hashCode(value);
+        }
+
+        public final V setValue(V newValue) {
+            V oldValue = value;
+            value = newValue;
+            return oldValue;
+        }
+
+        public final boolean equals(Object o) {
+            if (o == this)
+                return true;
+            if (o instanceof Map.Entry) {
+                Map.Entry<?, ?> e = (Map.Entry<?, ?>) o;
+                if (Objects.equals(key, e.getKey()) &&
+                        Objects.equals(value, e.getValue()))
+                    return true;
+            }
+            return false;
+        }
+    }
+
+}
---
+++ src/main/java/com/simon/java/sourcecodeanalysis/StringSca.java
@@ -0,0 +1,84 @@
+package com.simon.java.sourcecodeanalysis;
+
+public class StringSca {
+
+    public static void main(String[] args) {
+        var str1 = "HaHa";
+        var str2 = new String("HaHa");
+        char[] ch = {'H', 'a', 'H', 'a'};
+        var str3 = new String(ch);
+
+        System.out.println(str1.equals(str2)); //true
+        System.out.println(str1.equals(str3)); //true
+        System.out.println(str2.equals(str3)); //true
+
+        System.out.println(str1 == str2); //false
+        System.out.println(str1 == str3); //false
+        System.out.println(str2 == str3); //false
+        System.out.println("-------------");
+
+        /*
+        String 常见的创建方式有两种，new String() 的方式和直接赋值的方式，直接赋值的方式会先去字符串常量池中查找是否已经有此值，
+        如果有则把引用地址直接指向此值，否则会先在常量池中创建，然后再把引用指向此值；而 new String() 的方式一定会先在堆上创建一个字符串对象，
+        然后再去常量池中查询此字符串的值是否已经存在，如果不存在会先在常量池中创建此字符串，然后把引用的值指向此字符串
+         */
+        var s1 = new String("Java");
+        //如果常量池中存在当前字符串, 就会直接返回当前字符串. 如果常量池中没有此字符串, 会将此字符串放入常量池中后, 再返回
+        var s2 = s1.intern();
+        var s3 = "Java";
+        System.out.println(s1 == s2); //false
+        System.out.println(s2 == s3); //true
+        System.out.println(s1.equals(s2));  //true
+        System.out.println(s2.equals(s3));  //true
+    }
+
+
+    /*
+    一、String 被final修饰的原因：
+    Java 语言之父 James Gosling 的回答是，他会更倾向于使用 final，因为它能够缓存结果，当你在传参时不需要考虑谁会修改它的值；
+    如果是可变类的话，则有可能需要重新拷贝出来一个新值进行传参，这样在性能上就会有一定的损失。
+    James Gosling 还说迫使 String 类设计成不可变的另一个原因是安全，当你在调用其他方法时，比如调用一些系统级操作指令之前，可能会有一系列校验，
+    如果是可变类的话，可能在你校验过后，它的内部的值又被改变了，这样有可能会引起严重的系统崩溃问题，这是迫使 String 类设计成不可变类的一个重要原因。
+    总结来说，使用 final 修饰的第一个好处是安全；第二个好处是高效。
+    只有字符串是不可变时，我们才能实现字符串常量池，字符串常量池可以为我们缓存字符串，提高程序的运行效率
+
+    二、String 变更  两大改善，存储由char[] 变为byte[]数组，存储变得更为紧凑可以更小的内存空间、操作性能更高性
+    public boolean equals(Object anObject) {
+    // 对象引用相同直接返回 true
+    if (this == anObject) {
+        return true;
+    }
+    JDK8实现
+    // 判断需要对比的值是否为 String 类型，如果不是则直接返回 false
+    if (anObject instanceof String) {
+        String anotherString = (String)anObject;
+        int n = value.length;
+        if (n == anotherString.value.length) {
+            // 把两个字符串都转换为 char 数组对比
+            char v1[] = value;
+            char v2[] = anotherString.value;
+            int i = 0;
+            // 循环比对两个字符串的每一个字符
+            while (n-- != 0) {
+                // 如果其中有一个字符不相等就 true false，否则继续对比
+                if (v1[i] != v2[i])
+                    return false;
+                i++;
+            }
+            return true;
+        }
+    }
+    JDK9以后 引入了StringLatin1 和 StringUTF16 进行处理
+    if (anObject instanceof String) {
+            String aString = (String)anObject;
+            if (coder() == aString.coder()) {
+                return isLatin1() ? StringLatin1.equals(value, aString.value)
+                                  : StringUTF16.equals(value, aString.value);
+            }
+    }
+    return false;
+    }
+    */
+
+
+}
