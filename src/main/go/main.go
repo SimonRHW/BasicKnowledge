a86e70a2871d0a4d43c7cfb7a8d64221696af200
Hongwei.Ren <renhongwe@gmail.com>
2022-12-02 23:10:55 +0800

    add go file

---
+++ src/main/go/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func findAverage(a []int) float64 {
+	count := 4
+	sum := 0
+	for i := 0; i < count; i++ {
+		sum += (a[i])
+	}
+
+	return float64(sum/count)
+}
+
+func main() {
+	i := []int{4, 6, 8, 10}
+	fmt.Println("AVERAGE", findAverage(i))
+}
