a715eb53e0c15fc620e1071956036d4cf8429da8
Hongwei.Ren <renhongwe@gmail.com>
2022-12-02 18:29:04 +0800

    add rust moudle

---
+++ src/main/java/com/simon/java/suspendcps/Debug.java
@@ -0,0 +1,14 @@
+package com.simon.java.suspendcps;
+
+import java.util.concurrent.ExecutionException;
+
+public class Debug {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        JavaWithContinuation javaWithContinuation = new JavaWithContinuation();
+        javaWithContinuation.testGetUserInfoAsync();
+        javaWithContinuation.testGetUserInfoJFlow();
+        javaWithContinuation.testGetUserInfoAsync();
+
+    }
+}
